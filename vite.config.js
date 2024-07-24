8ba551dbcbd43d0f34a6e703ff5f5be1fd08e69b
Luz Lopez <centralpuntosas@gmail.com>
2024-07-24 16:50:49 -0500

    mi tercer commid

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/app.js',
+                //editando para validar cambios 
+                
                 
             ],
             refresh: true,
